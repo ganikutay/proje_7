887307e22e17921595653545f4d3b9d55a8f58b0
ganikutay <85530234+ganikutay@users.noreply.github.com>
2023-11-03 00:39:30 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Form2 from './components/Form_2';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Form2 />
+    </div>
+  );
+}
+
+export default App;
